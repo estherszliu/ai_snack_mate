5d19b8ee03de1237b04aad5fe4407e41ddc25d49
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-06-04 14:29:23 +1000

    fix:rename product

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div id="homeContainer">
       <h1 id="homeTitle">Welcome to Snack Mate</h1>
-      <p id="homeDescription">Discover the joy of cooking with our AI-powered recipe generator. Enter any dish, and we'll provide a delicious recipe tailored just for you!</p>
+      <p id="homeDescription">Discover the joy of cooking with our powered recipe generator. Enter any dish, and we'll provide a delicious recipe tailored just for you!</p>
       <div id="homeImages">
         <img src="./delicious-food.jpg" alt="Delicious Food" className="homeImage"/>
         <img src="./cooking.jpg" alt="Cooking" className="homeImage"/>
